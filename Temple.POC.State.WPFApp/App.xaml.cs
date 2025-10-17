cd360675eb7f6bea5559ad7fd1a292e6a0f6fabf
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-10-17 15:06:03 +0200

    -

---
+++ Temple.POC.State.WPFApp/App.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using Temple.Application;
+using Temple.Application.Core;
+using Temple.Application.Interfaces;
+using Temple.Application.Smurfs;
+using Temple.Infrastructure.Pagination;
+using Temple.Persistence;
+using Temple.Persistence.EFCore.AppData;
+
+namespace Temple.POC.State.WPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        private IHost _host;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var splash = new SplashScreenWindow();
+            splash.Show();
+
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    //var connectionString = "Data source=babuska27.db";
+                    //var connectionString = "Server=localhost;Port=5432;User Id=root;Password=root;Database=DB_DummyWpfApp";
+                    var connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=L1on8Zebra;Database=DB_WPF_POC";
+
+                    services.AddAppDataPersistence<PRDbContextBase>(options =>
+                    {
+                        //options.UseSqlite(connectionString);
+                        options.UseNpgsql(connectionString);
+                    });
+
+                    services.AddAutoMapper(assemblies: typeof(MappingProfiles).Assembly);
+                    services.AddApplication();   // registers MediatR and handlers
+                    services.AddScoped<IUnitOfWorkFactory, UnitOfWorkFactory>();
+                    services.AddScoped<IPagingHandler<SmurfDto>, PagingHandler<SmurfDto>>();
+
+                    services.AddMediatR(cfg =>
+                        cfg.RegisterServicesFromAssemblyContaining<List.Query>());
+
+                    // Register our ViewModel and View
+                    services.AddSingleton<MainWindowViewModel>();
+                    services.AddTransient<MainWindow>();
+                })
+                .Build();
+
+            Task.Run(async () =>
+            {
+                using var scope = _host.Services.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<PRDbContextBase>();
+                db.Database.MigrateAsync().GetAwaiter().GetResult();
+                Seeding.SeedDatabase(db).GetAwaiter().GetResult();
+
+                await Current.Dispatcher.InvokeAsync(() =>
+                {
+                    var scope = _host!.Services.CreateScope();
+                    var mainWindow = scope.ServiceProvider.GetRequiredService<MainWindow>();
+                    splash.Close();
+                    mainWindow.Show();
+                });
+            });
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            if (_host is not null)
+            {
+                await _host.StopAsync();
+            }
+
+            _host?.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ Temple.POC.State.WPFApp/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MediatR;
+using System.Collections.ObjectModel;
+using Temple.Application.Smurfs;
+
+namespace Temple.POC.State.WPFApp
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _buttonText;
+        private readonly IMediator _mediator;
+
+        public RelayCommand LoadSmurfsCommand { get; }
+
+        public string ButtonText
+        {
+            get => _buttonText;
+            set
+            {
+                _buttonText = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ObservableCollection<SmurfDto> Smurfs { get; } = new();
+
+        public MainWindowViewModel(
+            IMediator mediator)
+        {
+            _buttonText = "Populate list with smurfs";
+            _mediator = mediator;
+
+            LoadSmurfsCommand = new RelayCommand(async () => await LoadSmurfsAsync());
+        }
+
+        private async Task LoadSmurfsAsync()
+        {
+            var smurfs = await _mediator.Send(new List.Query { Params = new SmurfParams() });
+
+            Smurfs.Clear();
+            foreach (var smurfDto in smurfs.Value)
+            {
+                Smurfs.Add(smurfDto);
+            }
+        }
+    }
+}
