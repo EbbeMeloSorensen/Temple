8a66e9635bf4a23b6dfc48d574df86ef9aa5e6bb
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2025-10-26 16:53:32 +0100

    Added archers

--- Temple.ViewModel/DD/BattleViewModel.cs
+++ Temple.ViewModel/DD/BattleViewModel.cs
@@ -47,17 +47,58 @@ public class BattleViewModel : ViewModelBase
             dummyScene,
             logger);
 
-        var knight = new CreatureType(
-            "Knight", 20, 3, 12, 0, 4, new List<Attack>
+        var knight = new CreatureType("Knight",
+            maxHitPoints: 20,
+            armorClass: 3,
+            thaco: 12,
+            initiativeModifier: 0,
+            movement: 4,
+            attacks:  new List<Attack>
             {
                 new MeleeAttack("Longsword", 10),
                 new MeleeAttack("Longsword", 10)
             });
 
-         var goblin = new CreatureType(
-            "Goblin", 12, 5, 20, 0, 6, new List<Attack>
+        var goblin = new CreatureType(
+            name: "Goblin",
+            maxHitPoints: 12,
+            armorClass: 5,
+            thaco: 20,
+            initiativeModifier: 0,
+            movement: 6,
+            attacks: new List<Attack>
             {
                 new MeleeAttack("Short sword", 6)
+            });
+
+        var archer = new CreatureType(
+            name: "Archer",
+            maxHitPoints: 12,
+            armorClass: 6,
+            thaco: 14,
+            initiativeModifier: 10,
+            movement: 6,
+            attacks: new List<Attack>
+            {
+                new RangedAttack(
+                    name: "Bow & Arrow",
+                    maxDamage: 4,
+                    range: 5)
+            });
+
+        var goblinArcher = new CreatureType(
+            name: "Goblin Archer",
+            maxHitPoints: 20,
+            armorClass: 7,
+            thaco: 13,
+            initiativeModifier: 0,
+            movement: 6,
+            attacks: new List<Attack>
+            {
+                new RangedAttack(
+                    name: "Bow & Arrow",
+                    maxDamage: 4,
+                    range: 4)
             });
 
         var scene = new Scene("DummyScene", 4, 4);
@@ -66,7 +107,9 @@ public class BattleViewModel : ViewModelBase
         scene.AddObstacle(new Obstacle(ObstacleType.Water, 2, 1));
         scene.AddObstacle(new Obstacle(ObstacleType.Water, 2, 2));
         scene.AddCreature(new Creature(knight, false) { IsAutomatic = false }, 0, 0);
-        scene.AddCreature(new Creature(goblin, true) { IsAutomatic = true }, 3, 3);
+        scene.AddCreature(new Creature(archer, false) { IsAutomatic = false }, 0, 1);
+        scene.AddCreature(new Creature(goblin, true) { IsAutomatic = true }, 3, 2);
+        scene.AddCreature(new Creature(goblinArcher, true) { IsAutomatic = true }, 3, 3);
 
         dummyScene.Object = scene;
     }
