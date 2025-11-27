234676c5ed19f09ba2c55d363200f830a48a0b0e
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2025-11-27 21:18:10 +0100

    -

--- Temple.ViewModel/DD/ExploreAreaViewModel.cs
+++ Temple.ViewModel/DD/ExploreAreaViewModel.cs
@@ -82,134 +82,19 @@ namespace Temple.ViewModel.DD
                 GeometryEditorViewModel,
                 shapeSelectorCallback,
                 shapeUpdateCallback);
+        }
 
-            var scene = GenerateScene();
-
+        public void StartAnimation(
+            Scene scene)
+        {
             GeometryEditorViewModel.InitializeWorldWindow(
                 scene.InitialWorldWindowFocus(),
                 scene.InitialWorldWindowSize(),
                 false);
 
             _sceneViewController.ActiveScene = scene;
-        }
 
-        public void StartAnimation()
-        {
             Engine.StartOrResumeAnimation();
-        }
-
-        private Scene GenerateScene()
-        {
-            var ballRadius = 0.16;
-            var initialBallPosition = new Vector2D(0, 0);
-
-            var initialState = new State();
-            initialState.AddBodyState(
-                new BodyStateClassic(new CircularBody(1, ballRadius, 1, false), initialBallPosition)
-                {
-                    Orientation = Math.PI
-                });
-
-            var name = "Exploration";
-            var standardGravity = 0.0;
-            var initialWorldWindowUpperLeft = new Point2D(-1.4, -1.3);
-            var initialWorldWindowLowerRight = new Point2D(5, 3);
-            var gravitationalConstant = 0.0;
-            var coefficientOfFriction = 0.0;
-            var timeFactor = 1.0;
-            var handleBodyCollisions = false;
-            var deltaT = 0.001;
-            var viewMode = SceneViewMode.FocusOnFirstBody;
-
-            var scene = new Scene(
-                name,
-                initialWorldWindowUpperLeft,
-                initialWorldWindowLowerRight,
-                initialState,
-                standardGravity,
-                gravitationalConstant,
-                coefficientOfFriction,
-                timeFactor,
-                handleBodyCollisions,
-                deltaT,
-                viewMode);
-
-            scene.CollisionBetweenBodyAndBoundaryOccuredCallBack =
-                body => OutcomeOfCollisionBetweenBodyAndBoundary.Block;
-
-            scene.InteractionCallBack = (keyboardState, keyboardEvents, mouseClickPosition, collisions, currentState) =>
-            {
-                var currentStateOfMainBody = currentState.BodyStates.First() as BodyStateClassic;
-                var currentRotationalSpeed = currentStateOfMainBody.RotationalSpeed;
-                var currentArtificialSpeed = currentStateOfMainBody.ArtificialVelocity.Length;
-
-                var newRotationalSpeed = 0.0;
-
-                if (keyboardState.LeftArrowDown)
-                {
-                    newRotationalSpeed += Math.PI;
-                }
-
-                if (keyboardState.RightArrowDown)
-                {
-                    newRotationalSpeed -= Math.PI;
-                }
-
-                var newArtificialSpeed = 0.0;
-
-                if (keyboardState.UpArrowDown)
-                {
-                    newArtificialSpeed += 1.5;
-                }
-
-                if (keyboardState.DownArrowDown)
-                {
-                    newArtificialSpeed -= 1.5;
-                }
-
-                currentStateOfMainBody.RotationalSpeed = newRotationalSpeed;
-                currentStateOfMainBody.ArtificialVelocity = new Vector2D(newArtificialSpeed, 0);
-
-                if (Math.Abs(newRotationalSpeed - currentRotationalSpeed) < 0.01 &&
-                    Math.Abs(newArtificialSpeed - currentArtificialSpeed) < 0.01)
-                {
-                    return false;
-                }
-
-                return true;
-            };
-
-            // Liniestykker defineres i et normalt xy koordinatsystem
-            var lineSegments = new List<LineSegment2D>
-            {
-                new(new Point2D(-2, 2), new Point2D(-2, 0)),
-                new(new Point2D(-3, 0), new Point2D(-3, -4)),
-                new(new Point2D(-2, 0), new Point2D(-3, 0)),
-                new(new Point2D(2, 2), new Point2D(-2, 2)),
-                new(new Point2D(2, -2), new Point2D(2, 2)),
-                new(new Point2D(-2, -2), new Point2D(2, -2)),
-            };
-
-            //var group = new Model3DGroup();
-            //var material = new DiffuseMaterial(new SolidColorBrush(Colors.Orange));
-
-            foreach (var lineSegment in lineSegments)
-            {
-                scene.AddBoundary(new LineSegment(
-                    new Vector2D(lineSegment.Point1.X, -lineSegment.Point1.Y),
-                    new Vector2D(lineSegment.Point2.X, -lineSegment.Point2.Y)));
-
-                //var rectangleMesh = CreateWall(
-                //    new Point2D(lineSegment.Point1.Y, lineSegment.Point1.X),
-                //    new Point2D(lineSegment.Point2.Y, lineSegment.Point2.X));
-
-                //var rectangleModel = new GeometryModel3D(rectangleMesh, material);
-                //group.Children.Add(rectangleModel);
-            }
-
-            //Scene3D = group;
-
-            return scene;
         }
     }
 }
