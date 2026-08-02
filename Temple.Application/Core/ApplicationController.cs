7aa7e765e9c796f71b834d50085eb59a222399e1
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-08-02 14:23:28 +0200

    -

--- Temple.Application/Core/ApplicationController.cs
+++ Temple.Application/Core/ApplicationController.cs
@@ -112,7 +112,8 @@ public class ApplicationController
         GeneratePartyData();
 
         // Magic number
-        ApplicationData.CurrentSiteId = "village";
+        //ApplicationData.CurrentSiteId = "village";
+        ApplicationData.CurrentSiteId = "undermountain";
 
         _applicationStateMachine.NextPayload = new InterludePayload
         {
